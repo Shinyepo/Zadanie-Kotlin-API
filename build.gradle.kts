78893f3d9c973b04e21a846f0c5e664bf3fd6df1
Shinyepo <shinyepo42@gmail.com>
2025-04-26 19:00:30 +0200

    Add dependencies, cleanup Main.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+val ktorVersion: String by project
+val logbackVersion: String by project
+
 plugins {
     kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 group = "dev.shinyepo"
@@ -10,6 +14,12 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:${ktorVersion}")
+    implementation("io.ktor:ktor-client-cio:${ktorVersion}")
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+    implementation("io.ktor:ktor-client-content-negotiation:${ktorVersion}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
+
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
 package dev.shinyepo
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import kotlinx.coroutines.runBlocking
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+fun main() {
+    val client = HttpClient(CIO)
+
+    runBlocking {
+        val request: HttpResponse = client.get("https://jsonplaceholder.typicode.com/posts")
+
+        println(request.status)
     }
 }
