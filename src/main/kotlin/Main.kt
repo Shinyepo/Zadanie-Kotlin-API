2c5ebf8c5f8691607dfa3714f5f146d23b3f20ec
Shinyepo <shinyepo42@gmail.com>
2025-04-26 20:35:40 +0200

    Add fetch and save to file logic for posts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
 package dev.shinyepo
 
 import dev.shinyepo.services.PostService
+import dev.shinyepo.utils.FileUtils
 
 suspend fun main() {
+
+    println("Fetching posts...")
     val posts = PostService().fetchAllPosts()
+    println("Fetched ${posts.size} posts.")
+
+    if (posts.isNotEmpty()) {
+        println("Saving posts to ~/posts/<post_id>.json")
+        for (post in posts) {
+            FileUtils.savePostToFile(post)
+        }
+        println("Done.")
+    }
+
 }
