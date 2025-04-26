b13855db396f6e37d76d3caab16c98d2a47476e5
Shinyepo <shinyepo42@gmail.com>
2025-04-26 20:35:20 +0200

    Add FileUtils

---
+++ src/main/kotlin/utils/FileUtils.kt
@@ -0,0 +1,24 @@
+package dev.shinyepo.utils
+
+import dev.shinyepo.models.Post
+import kotlinx.serialization.json.Json
+import java.io.File
+
+object FileUtils {
+
+    fun savePostToFile(post: Post) {
+        val formattedString = Json{ prettyPrint = true }.encodeToString(post)
+
+        val dir = File("posts")
+        if (!dir.exists()) {
+            dir.mkdir()
+        }
+
+        val file = File(dir, "${post.id}.json")
+        if (file.exists()) {
+            println("File with post id ${post.id} already exists. Skipping...")
+            return
+        }
+        file.writeText(formattedString)
+    }
+}
