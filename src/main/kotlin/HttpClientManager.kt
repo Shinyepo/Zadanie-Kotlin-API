57def311b3a27b5dcf2bf2026d5cf0e59d7da8a8
Shinyepo <shinyepo42@gmail.com>
2025-04-26 20:07:45 +0200

    Add HttpClientManager

---
+++ src/main/kotlin/HttpClientManager.kt
@@ -0,0 +1,17 @@
+package dev.shinyepo
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientManager {
+    val Client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+}
