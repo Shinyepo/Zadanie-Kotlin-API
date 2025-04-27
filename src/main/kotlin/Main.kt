5db6c2b080b28b980cb8d19bd56a73de643f6e9c
Shinyepo <shinyepo42@gmail.com>
2025-04-27 16:17:42 +0200

    Add safeApiCall with proper Success/Failure handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,16 +7,19 @@ import org.slf4j.LoggerFactory
 private val logger = LoggerFactory.getLogger("dev.shinyepo.main")
 
 suspend fun main() {
+    PostService()
+        .fetchAllPosts()
+        .onSuccess { posts ->
+            logger.info("Fetched ${posts.size} posts.")
+            logger.info("Saving posts to ~/posts/<post_id>.json")
 
-    val posts = PostService().fetchAllPosts()
-    logger.info("Fetched ${posts.size} posts.")
+            for (post in posts) {
+                FileUtils.savePostToFile(post)
+            }
 
-    if (posts.isNotEmpty()) {
-        logger.info("Saving posts to ~/posts/<post_id>.json")
-        for (post in posts) {
-            FileUtils.savePostToFile(post)
+            logger.info("Done.")
         }
-        logger.info("Done.")
-    }
-
+        .onFailure { exception ->
+            logger.error(exception.localizedMessage)
+        }
 }
--- src/main/kotlin/services/PostService.kt
+++ src/main/kotlin/services/PostService.kt
@@ -2,6 +2,7 @@ package dev.shinyepo.services
 
 import dev.shinyepo.network.HttpClientManager
 import dev.shinyepo.models.Post
+import dev.shinyepo.network.ApiOperation
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import org.slf4j.LoggerFactory
@@ -9,15 +10,12 @@ import org.slf4j.LoggerFactory
 class PostService() {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    suspend fun fetchAllPosts(): List<Post> {
-        return try {
+    suspend fun fetchAllPosts(): ApiOperation<List<Post>> {
+        return HttpClientManager.safeApiCall {
             logger.info("Fetching all posts...")
             HttpClientManager.client.use { client ->
                 client.get("https://jsonplaceholder.typicode.com/posts").body<List<Post>>()
             }
-        } catch (e: Exception) {
-            logger.error("Encountered error while fetching posts: \n" + e.localizedMessage)
-            emptyList()
         }
     }
 }
